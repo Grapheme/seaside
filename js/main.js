ac616058a16446bbf49d6ce1e3f809f598aef49f
KostyaD <thedamaxstudio@gmail.com>
2014-04-15 18:55:29 +0400

    Верстка

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-
+$(function () {
+	var $fotoramaDiv = $('.slider').fotorama();
+	var fotorama = $fotoramaDiv.data('fotorama');
+	$('.slider-prev').on('click', function(){
+		fotorama.show('<');
+	});
+	$('.slider-next').on('click', function(){
+		fotorama.show('>');
+	});
+});
